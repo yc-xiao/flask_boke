5654a131fc0cf9178f1b137a07779f9f33ebe46e
youcan <youcan.xiao@sagene.com.cn>
2019-11-19 09:25:58 +0800

    上传功能更新

--- app/secure.py
+++ app/secure.py
@@ -2,6 +2,6 @@ SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:333333@localhost:3306/boke'
 
 
 SECRET_KEY = '\x88D\xf09\x91\x07\x98\x89\x87\x96\xa0A\xc68\xf9\xecJ:U\x17\xc5V\xbe\x8b\xef\xd7\xd8\xd3\xe6\x98*4'
-UPLOAD_FOLDER = '/home/share/files/'
-FILE_URL = 'http://localhost'
+FILE_URL = 'http://127.0.0.1:8888/'
+UPLOAD_PATH = r'/home/youcan/'
 MAX_CONTENT_LENGTH = 2 * 1024 * 1024 * 1024  # 2GB
--- app/web/upload.py
+++ app/web/upload.py
@@ -1,19 +1,54 @@
 from flask import render_template, request, url_for, redirect
 from flask_login import current_user, login_required
 from werkzeug import secure_filename
-from app.secure import UPLOAD_FOLDER, FILE_URL
+from app.secure import FILE_URL, UPLOAD_PATH
 from .base import web
 import os
+from http import server
 
 
-@web.route('/upload', methods=['GET','POST'])
+@web.route('/upload/', methods=['GET','POST'])
 def upload():
+    url = request.url
     if request.method == 'GET':
-        return render_template('upload.html')
+        files = os.listdir(UPLOAD_PATH)
+        temp_f_files, temp_d_files = [], []
+        for file in files:
+            path = UPLOAD_PATH + file
+            if os.path.isfile(path):
+                temp_f_files.append({'url':f'{FILE_URL}{file}', 'name': file})
+            else:
+                temp_d_files.append({'url':f'{url}{file}/', 'name': f'{file}/'})
+            temp_files = temp_d_files + temp_f_files
+        return render_template('upload.html', upload_url=url, files=temp_files)
     if request.method == 'POST':
         file = request.files['files']
         if file:
             filename = secure_filename(file.filename)
-            file.save(os.path.join(UPLOAD_FOLDER, filename))
-            return redirect(FILE_URL)
+            file.save(os.path.join(UPLOAD_PATH, filename))
+            return redirect(url_for('web.upload'))
+        return redirect(url_for('web.upload'))
+
+@web.route('/upload/<path:file_path>', methods=['GET','POST'])
+def upload2(file_path=None):
+    url = request.url
+    if request.method == 'GET':
+        upload_path = UPLOAD_PATH + file_path
+        files = os.listdir(upload_path)
+        temp_f_files, temp_d_files = [], []
+        for file in files:
+            path = upload_path + file
+            if os.path.isfile(path):
+                temp_f_files.append({'url':f'{FILE_URL}{file_path}{file}', 'name': file})
+            else:
+                temp_d_files.append({'url':f'{url}{file}/', 'name': f'{file}/'})
+            temp_files = temp_d_files + temp_f_files
+        return render_template('upload.html', upload_url=url, files=temp_files)
+
+    if request.method == 'POST':
+        file = request.files['files']
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(f'{UPLOAD_PATH}/{file_path}', filename))
+            return redirect(url)
         return redirect(url_for('web.upload'))
