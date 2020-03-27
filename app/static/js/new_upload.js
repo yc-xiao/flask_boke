3c844e3ca8d1ac259f5dfed057a35cbdae97f833
youcan <youcan.xiao@sagene.com.cn>
2020-03-27 11:33:17 +0800

    文件断点上传，同步

---
+++ app/static/js/new_upload.js
@@ -0,0 +1,176 @@
+//上传文件方法
+function UpladFile() {
+    var fileObj = document.getElementById("file").files[0]; // js 获取文件对象
+    search(fileObj);
+}
+
+function search(fileObj){
+    var url =  "http://localhost:5000/new_upload/?"; // 接收上传文件的后台地址
+    url = url + 'file_name' + '='+ fileObj.name + '&';
+    url = url + 'file_size' + '='+ fileObj.size + '&';
+    url = url + 'last_time' + '='+ fileObj.lastModified;
+    // 查询文件状态
+    var xhr = new XMLHttpRequest();  // XMLHttpRequest 对象
+    xhr.open('get', url, true); //post方式，url为服务器请求地址，true 该参数规定请求是否异步处理。
+    xhr.onload = function(evt){
+        //服务断接收完文件返回的结果
+        var data = JSON.parse(evt.target.responseText);
+        if(!data['status']){
+            handler_search(data);
+        }
+        update_process(data);
+    }; //请求完成
+
+    xhr.onerror = function(evt){
+        console.log("上传失败！");
+    }; //请求失败
+
+    xhr.send(); //开始上传，发送form数据
+}
+
+var a = 1;
+function handler_search(data){
+    blocks = data['blocks'];
+    console.log(a);
+    a+=1;
+    var datas;
+    for(var each in blocks){
+        if(!blocks[each]['status']){
+            var datas = get_datas(each, blocks[each]['start'], blocks[each]['end']);
+            url = "http://localhost:5000/new_upload/";
+            var xhr = new XMLHttpRequest();  // XMLHttpRequest 对象
+            xhr.open('post', url, true); //post方式，url为服务器请求地址，true 该参数规定请求是否异步处理。
+            xhr.onload = function(evt){
+                //服务断接收完文件返回的结果
+                var data = JSON.parse(evt.target.responseText);
+                if(!data['status']){
+                    handler_search(data);
+                    update_process(data);
+                }
+                update_process(data);
+            }; //请求完成
+            xhr.onerror = function(evt){
+                console.log("上传失败！");
+            }; //请求失败
+            xhr.send(datas); //开始上传，发送form数据
+            break;
+        }
+    }
+}
+
+// function handler_searchx(data){
+//     blocks = data['blocks'];
+//     for(var each in blocks){
+//         if(!blocks[each]['status']){
+//             var datas = get_datas(each, blocks[each]['start'], blocks[each]['end']);
+//             url = "http://localhost:5000/new_upload/";
+//             var xhr = new XMLHttpRequest();  // XMLHttpRequest 对象
+//             xhr.open('post', url, true); //post方式，url为服务器请求地址，true 该参数规定请求是否异步处理。
+//             xhr.onload = function(evt){
+//                 //服务断接收完文件返回的结果
+//                 var data = JSON.parse(evt.target.responseText);
+//
+//             }; //请求完成
+//             xhr.onerror = function(evt){
+//                 console.log("上传失败！");
+//             }; //请求失败
+//             xhr.send(datas); //开始上传，发送form数据
+//
+//         }
+//     }
+// }
+
+function get_datas(split_file_name, start, end){
+    var fileObj = document.getElementById("file").files[0];
+    files = fileObj.slice(start, end);
+    var form = new FormData(); // FormData 对象
+    form.append("files", files); // 文件对象
+    form.append("file_name", fileObj.name); // 文件对象
+    form.append("split_file_name", split_file_name); // 文件对象
+    form.append("last_time", fileObj.lastModified); // 文件对象
+    return form
+}
+
+//上传成功响应
+function searchComplete(evt) {
+    //服务断接收完文件返回的结果
+    var data = JSON.parse(evt.target.responseText);
+    if(!data['status']){
+        handler_search(data);
+    }
+}
+
+function update_process(data){
+    blocks = data['blocks'];
+    var count = 0;
+    var total = 0;
+    for(var each in blocks){
+        total+=1;
+        if(blocks[each]['status']){
+            count+=1;
+        }else{
+            console.log(blocks[each]['status'])
+        }
+    }
+    var progressBar = document.getElementById("progressBar");
+    var percentageDiv = document.getElementById("percentage");
+    progressBar.max = total;
+    progressBar.value = count;
+    percentageDiv.innerHTML = '上传进度:' + count/total;
+}
+
+//上传失败
+// function uploadFailed(evt) {
+//     console.log("上传失败！");
+// }
+
+//取消上传
+// function cancleUploadFile(){
+//     xhr.abort();
+// }
+
+// function http(method, url, data, onprogress){
+//     var xhr;
+//     xhr = new XMLHttpRequest();  // XMLHttpRequest 对象
+//     xhr.open(method, url, true); //post方式，url为服务器请求地址，true 该参数规定请求是否异步处理。
+//     xhr.onload = uploadComplete; //请求完成
+//     xhr.onerror =  uploadFailed; //请求失败
+//     xhr.upload.onprogress = onprogress;//【上传进度调用方法实现】
+//     xhr.send(data); //开始上传，发送form数据
+// }
+//
+
+//上传进度实现方法，上传过程中会频繁调用该方法
+// function progressFunction(evt) {
+//     var progressBar = document.getElementById("progressBar");
+//     var percentageDiv = document.getElementById("percentage");
+//     // event.total是需要传输的总字节，event.loaded是已经传输的字节。如果event.lengthComputable不为真，则event.total等于0
+//     if (evt.lengthComputable) {//
+//         progressBar.max = evt.total;
+//         progressBar.value = evt.loaded;
+//         percentageDiv.innerHTML = Math.round(evt.loaded / evt.total * 100) + "%";
+//     }
+//     var time = document.getElementById("time");
+//     var nt = new Date().getTime();//获取当前时间
+//     var pertime = (nt-ot)/1000; //计算出上次调用该方法时到现在的时间差，单位为s
+//     ot = new Date().getTime(); //重新赋值时间，用于下次计算
+//     var perload = evt.loaded - oloaded; //计算该分段上传的文件大小，单位b
+//     oloaded = evt.loaded;//重新赋值已上传文件大小，用以下次计算
+//     //上传速度计算
+//     var speed = perload/pertime;//单位b/s
+//     var bspeed = speed;
+//     var units = 'b/s';//单位名称
+//     if(speed/1024>1){
+//         speed = speed/1024;
+//         units = 'k/s';
+//     }
+//     if(speed/1024>1){
+//         speed = speed/1024;
+//         units = 'M/s';
+//     }
+//     speed = speed.toFixed(1);
+//     //剩余时间
+//     var resttime = ((evt.total-evt.loaded)/bspeed).toFixed(1);
+//     time.innerHTML = '，速度：'+speed+units+'，剩余时间：'+resttime+'s';
+//     if(bspeed==0) time.innerHTML = '上传已取消';
+// }
--- app/web/upload.py
+++ app/web/upload.py
@@ -2,6 +2,7 @@ from flask import render_template, request, url_for, redirect, jsonify, make_res
 from flask_login import current_user, login_required
 from werkzeug import secure_filename
 import xlrd, csv
+import json
 import os
 
 from app.secure import FILE_URL, UPLOAD_PATH, IP_HOST
@@ -10,6 +11,7 @@ from .base import web
 
 @web.route('/upload/', methods=['GET','POST'])
 def upload():
+    # 上传首页
     url = request.url
     url = url.replace('http://localhost:5000', IP_HOST)
     # url = url.replace('localhost:5000', IP_HOST)
@@ -36,6 +38,7 @@ def upload():
 
 @web.route('/upload/<path:file_path>', methods=['GET','POST'])
 def upload2(file_path=None):
+    # 上传2
     url = request.url
     url = url.replace('http://localhost:5000', IP_HOST)
     # url = url.replace('localhost:5000', IP_HOST)
@@ -65,6 +68,7 @@ def upload2(file_path=None):
 
 @web.route('/uploads/<file_type>/', methods=['POST'])
 def upload_file(file_type=None):
+    # 上传文件并解析
     results = {'test': True, 'datas':[]}
     print(results, file_type)
     file_obj = request.files['files']
@@ -80,3 +84,104 @@ def upload_file(file_type=None):
         datas = datas.split('\r\n')
         results['datas'] = datas
     return make_response(jsonify(results), 200)
+
+@web.route('/new_upload_html/', methods=['GET'])
+def new_upload_html():
+    return render_template('new_upload.html')
+
+@web.route('/new_upload/', methods=['GET','POST'])
+def new_upload():
+    # filename/
+    #     filename_type.json
+    #     filename.type 文件
+    #     filename_type1 文件子文件
+    split_count = 10
+    # 新的上传，可断点重传
+    res = request
+    root_path = UPLOAD_PATH
+    if request.method == 'GET':
+        # import pdb;pdb.set_trace()
+        file_full_name = request.args['file_name']
+        file_size = int(request.args['file_size'])
+        last_time = request.args['last_time']
+        split_count = request.args.get('split_count', split_count)
+        *filename, file_type = file_full_name.split('.')
+        file_name = '.'.join(filename)
+
+        # /filename/filename_filetype.json
+        # 查看当前文件所在目录是否有json文件
+        file_dir = f'{root_path}{file_name}/'
+        split_file_name = f'{file_name}_{file_type}'
+        file_json = f'{file_dir}{split_file_name}.json'
+
+        if os.path.exists(file_json):
+            with open(file_json, 'r') as f:
+                data = f.read()
+            data = json.loads(data)
+        else:
+            data = {'name': file_name, 'type': file_type, 'size':file_size,
+                'last_time': last_time, 'status': False, 'blocks':{}}
+            block = int(file_size)//split_count
+            blocks = {
+                f'{split_file_name}.temp{i}': {'start': i*block, 'end': (i+1)*block, 'status': False}
+                for i in range(split_count-1)
+            }
+            blocks[f'{split_file_name}.temp{split_count-1}'] = {'start': (split_count-1)*block, 'end': file_size, 'status': False}
+            data['blocks'] = blocks
+            if not os.path.exists(file_dir):
+                os.mkdir(file_dir)
+            with open(file_json, 'w') as f:
+                f.write(json.dumps(data))
+        return make_response(jsonify(data), 200)
+    else:
+        # 查询后下载
+        file_full_name = request.form['file_name']
+        split_file_name = request.form['split_file_name']
+        last_time = request.form['last_time']
+        *filename, file_type = file_full_name.split('.')
+        file_name = '.'.join(filename)
+
+        # 查询文件是否修改
+        file_dir = f'{root_path}{file_name}/'
+        file_json_path = f'{file_dir}{file_name}_{file_type}.json'
+        if not os.path.exists(file_json_path):
+            return make_response('file_json_error', 400)
+
+        with open(file_json_path, 'r') as f:
+            file_json = json.loads(f.read())
+
+        if file_json['last_time'] != last_time:
+            return make_response('last_time_error', 400)
+
+        # 上传文件并保存
+        file = request.files['files']
+        if file:
+            file.save(os.path.join(f'{file_dir}', split_file_name))
+            file_json['blocks'][split_file_name]['status'] = True
+            with open(file_json_path, 'w') as f:
+                f.write(json.dumps(file_json))
+
+        # 检查文件是否全部更新,检查更新读取最新文件
+        with open(file_json_path, 'r') as f:
+            file_json = json.loads(f.read())
+
+        for value in file_json['blocks'].values():
+            if value['status'] == False:
+                return make_response(jsonify(file_json), 200)
+
+        # 全部上传，合并成一个
+        data = b''
+        for temp_file_name in file_json['blocks'].keys():
+            temp_file_path = f'{file_dir}{temp_file_name}'
+            with open(temp_file_path, 'rb') as f:
+                data+=f.read()
+
+        end_file_path = f'{file_dir}{file_name}.{file_type}'
+        print(end_file_path)
+        with open(end_file_path, 'wb') as  f:
+            f.write(data)
+
+        file_json['status'] = True
+        with open(file_json_path, 'w') as f:
+            f.write(json.dumps(file_json))
+        return make_response(jsonify(file_json), 200)
